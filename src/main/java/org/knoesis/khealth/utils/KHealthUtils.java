9cee855aa9af51a27b24546d2efe34a390bc3464
riccardotommasini <tomma156@gmail.com>
2015-11-17 01:51:37 -0500

    small refactoring

--- src/main/java/org/knoesis/khealth/utils/KHealthUtils.java
+++ src/main/java/org/knoesis/khealth/utils/KHealthUtils.java
@@ -1,7 +1,15 @@
 package org.knoesis.khealth.utils;
 
+import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
 
 public class KHealthUtils {
 	public static final String ontop_endpoint = "http://localhost:8080/openrdf-sesame/repositories/khealth-24h";
@@ -14,5 +22,17 @@ public class KHealthUtils {
 			+ "PREFIX r: <http://knoesis.org/ric#> "
 			+ "PREFIX asthma: <http://www.knoesis.org/khealth/asthma#> "
 			+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
-			+ "PREFIX k: <http://www.knoesis.org/khealth/>";
+			+ "PREFIX k: <http://www.knoesis.org/khealth/>"
+			+ "PREFIX time: <http://www.w3.org/2006/time#>";
+	
+	public static void debug(Model m){
+		Query sel = QueryFactory.create("SELECT * WHERE {?s ?p ?o}");
+		ResultSet res = QueryExecutionFactory.create(sel, m)
+				.execSelect();
+		ResultSetFormatter.out(System.out, res, sel);
+	}
+	
+	public static String now(){
+		return DateTime.now().toString(KHealthUtils.fmt);
+	}
 }
