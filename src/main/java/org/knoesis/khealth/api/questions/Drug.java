b9b5b93b76ff3b2842d3393335da338c3759cdb9
riccardotommasini <tomma156@gmail.com>
2015-11-17 21:21:57 -0500

    code cleaning

--- src/main/java/org/knoesis/khealth/api/questions/Drug.java
+++ src/main/java/org/knoesis/khealth/api/questions/Drug.java
@@ -9,7 +9,20 @@ import com.hp.hpl.jena.rdf.model.Model;
 
 public class Drug {
 
-	public static Model query() {
+	public static Model daylyQuery(DateTime d) {
+		return query(d, 1);
+	}
+
+	public static Model weeklyQuery(DateTime d) {
+		return query(d, 7);
+	}
+
+	public static Model query(DateTime d, int previous) {
+
+		return query(d.minusDays(previous), d);
+	}
+
+	public static Model query(DateTime from, DateTime to) {
 
 		String queryString = KHealthUtils.prefixes
 				+ "CONSTRUCT { ?p a :Patient ; asthma:hasAppliedMedication ?drug ; asthma:hasControlLevel ?cl . "
@@ -21,61 +34,11 @@ public class Drug {
 				+ "?obs ssn:featureOfInterest ?p ; ssn:observationResultTime ?instant ; ssn:observationResult ?res ; ssn:observedProperty ?drug ."
 				+ "?instant time:xsdDateTime ?time ."
 				+ "?drug a asthma:Drug . " + "?res ssn:hasValue ?val . "
-				+ "?val :hasObservationValue ?qv ."
-				+ "FILTER (?time > \"2015-06-16T00:00:00\"^^xsd:dateTime ) "
-				+ "FILTER (?time < \"2015-06-27T00:00:00\"^^xsd:dateTime) "
-				+ "FILTER ( ?qv = \"true\"^^xsd:boolean)" + "}";
-
-		// create and initialize repo
-
-		System.out.println(queryString);
-		QueryExecution sparqlService = QueryExecutionFactory.sparqlService(
-				KHealthUtils.ontop_endpoint, queryString);
-		// ResultSet res = sparqlService.execSelect();
-
-		Model m = sparqlService.execConstruct();
-		sparqlService.close();
-		return m;
-
-	}
-
-	public static Model daylyQuery(DateTime d) {
-		return query(d, 1);
-	}
-
-	public static Model weeklyQuery(DateTime d) {
-		return query(d, 7);
-	}
-
-	public static Model query(DateTime d, int previous) {
-
-		String prefixes = "PREFIX : <http://www.knoesis.org/khealth#> "
-				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-				+ "PREFIX ssn: <http://purl.oclc.org/NET/ssnx/ssn#> "
-				+ "PREFIX r: <http://knoesis.org/ric#> "
-				+ "PREFIX asthma: <http://www.knoesis.org/khealth/asthma#> "
-				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
-				+ "PREFIX time: <http://www.w3.org/2006/time#> ";
-		String from = d.minusDays(previous).toString(KHealthUtils.fmt);
-		String to = d.toString(KHealthUtils.fmt);
-		System.out.println("From " + from);
-		System.out.println("To " + to);
-		String queryString = prefixes
-				+ "CONSTRUCT { ?p a :Patient ; asthma:hasAppliedMedication ?drug ; asthma:hasControlLevel ?cl . "
-				+ "?cl a ?clt . "
-				+ "?drug a asthma:Drug ; :hasAssociatedTime ?time . } "
-				+ "WHERE { "
-				+ "?p asthma:hasControlLevel ?cl ."
-				+ "?cl a ?clt . "
-				+ "?obs ssn:featureOfInterest ?p ; ssn:observationResultTime ?instant ; ssn:observationResult ?res ; ssn:observedProperty ?drug ."
-				+ "?instant time:xsdDateTime ?time ."
-				+ "?drug a asthma:Drug . " + "?res ssn:hasValue ?val . "
 				+ "?val :hasObservationValue ?qv ." + "FILTER (?time > \""
-				+ from + "\"^^xsd:dateTime ) " + "FILTER (?time < \"" + to
+				+ from.toString(KHealthUtils.fmt) + "\"^^xsd:dateTime ) "
+				+ "FILTER (?time < \"" + to.toString(KHealthUtils.fmt)
 				+ "\"^^xsd:dateTime) "
 				+ "FILTER ( ?qv = \"true\"^^xsd:boolean)" + "}";
-
-		// create and initialize repo
 
 		System.out.println(queryString);
 		QueryExecution sparqlService = QueryExecutionFactory.sparqlService(
