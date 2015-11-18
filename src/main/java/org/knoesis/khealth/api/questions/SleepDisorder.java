5eb86bc4ae33c570a29c08792117f2a714cbe12e
riccardotommasini <tomma156@gmail.com>
2015-11-17 21:22:53 -0500

    code cleaning

--- src/main/java/org/knoesis/khealth/api/questions/SleepDisorder.java
+++ src/main/java/org/knoesis/khealth/api/questions/SleepDisorder.java
@@ -9,42 +9,20 @@ import com.hp.hpl.jena.rdf.model.Model;
 
 public class SleepDisorder {
 
-	public static Model query() {
-
-		String queryString = KHealthUtils.prefixes
-				+ "CONSTRUCT { ?p a :Patient ; asthma:hasSleepDisorder ?sleepdis . "
-				+ "?sleepdis a asthma:SleepDisorder ; :sleepdisDiagnosisTime ?time . } "
-				+ "WHERE { "
-				+ "?obs ssn:featureOfInterest ?p ; ssn:observationResultTime ?instant ; ssn:observationResult ?res ; ssn:observedProperty ?sleepdis ."
-				+ "?instant time:xsdDateTime ?time ."
-				+ "?sleepdis a asthma:SleepDisorder . "
-				+ "?res ssn:hasValue ?val . "
-				+ "?val :hasObservationValue ?qv ."
-				+ "FILTER (?time > \"2015-06-19T00:00:00\"^^xsd:dateTime ) "
-				+ "FILTER (?time < \"2015-06-21T00:00:00\"^^xsd:dateTime) "
-				+ "FILTER ( ?qv = \"true\"^^xsd:boolean)" + "}";
-
-		// create and initialize repo
-		System.err.println(queryString);
-
-		QueryExecution sparqlService = QueryExecutionFactory.sparqlService(
-				KHealthUtils.ontop_endpoint, queryString);
-		// ResultSet res = sparqlService.execSelect();
-
-		Model m = sparqlService.execConstruct();
-		sparqlService.close();
-		return m;
+	public static Model daylyQuery(DateTime d) {
+		return query(d, 1);
 	}
 
-	public static Model daylyQuery(DateTime d){
-		return query(d,1);
+	public static Model weeklyQuery(DateTime d) {
+		return query(d, 7);
 	}
-	
-	public static Model weeklyQuery(DateTime d){
-		return query(d,7);
-	}
-	
+
 	public static Model query(DateTime d, int previous) {
+
+		return query(d.minusDays(previous), d);
+	}
+
+	public static Model query(DateTime from, DateTime to) {
 
 		String prefixes = "PREFIX : <http://www.knoesis.org/khealth#> "
 				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
@@ -61,9 +39,10 @@ public class SleepDisorder {
 				+ "?instant time:xsdDateTime ?time ."
 				+ "?sleepdis a asthma:SleepDisorder . "
 				+ "?res ssn:hasValue ?val . "
-				+ "?val :hasObservationValue ?qv ."
-				+ "FILTER (?time > \""+d.minusDays(previous).toString(KHealthUtils.fmt)+"\"^^xsd:dateTime ) "
-				+ "FILTER (?time < \""+d.toString(KHealthUtils.fmt)+"\"^^xsd:dateTime) "
+				+ "?val :hasObservationValue ?qv ." + "FILTER (?time > \""
+				+ from.toString(KHealthUtils.fmt) + "\"^^xsd:dateTime ) "
+				+ "FILTER (?time < \"" + to.toString(KHealthUtils.fmt)
+				+ "\"^^xsd:dateTime) "
 				+ "FILTER ( ?qv = \"true\"^^xsd:boolean)" + "}";
 
 		// create and initialize repo
